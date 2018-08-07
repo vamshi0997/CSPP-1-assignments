a34ad226e1bb64504b12eaad114c2674dda679ee
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-07 19:29:56 +0530

    " -> 10 of 10 passed. pylint: 9/10 "

--- m8/p2/assignment2.py
+++ m8/p2/assignment2.py
@@ -4,19 +4,18 @@
 
 # This function takes in one number and returns one number.'''
 
-
 def sumofdigits(n1_):
     '''
     n is positive Integer
 
     returns: a positive integer, the sum of digits of n.
     '''
-    sum1 = 0
-    while n1_ >= 1:
+    if n1_ == 0:
+        return 0
+    elif n1_ >= 1:
         rem = n1_%10
-        sum1 = sum1+rem
         n1_ = n1_//10
-    return sum1
+        return rem + sumofdigits(n1_)
 
 
 def main():

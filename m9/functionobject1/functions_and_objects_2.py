1ce9801e351b603618a105a92ba1a5d74fe27ba6
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-08 17:39:28 +0530

    add1

---
+++ m9/functionobject1/functions_and_objects_2.py
@@ -0,0 +1,26 @@
+'''#Exercise : Function and Objects Exercise-2
+#Implement a function that converts the given testList = [1, -4, 8, -9]
+#into [2, -3, 9, -8]'''
+
+
+def inc(num):
+    '''
+    num: each list input
+    '''
+    return num+1
+
+def apply_to_each(l2_, fuc):
+    '''adding function to every list element'''
+    return list(map(fuc, l2_))
+
+def main():
+    '''data: any integer'''
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    print(apply_to_each(list1, inc))
+
+if __name__ == "__main__":
+    main()

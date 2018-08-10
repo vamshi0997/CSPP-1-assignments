3b704346b843c5664f37f4b5b4cbb2ed62f859dc
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-10 18:08:12 +0530

    " -> 9 of 9 passed. pylint: 8/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -22,13 +22,13 @@ def get_word_score(word, num):
     """
     res = 0
     word1 = word.lower()
-    score = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+    score1 = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
              'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
              's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 
     for i in word1:
-        if i in score:
-            res = res+(score[i]*len(word1))
+        if i in score1:
+            res = res+(score1[i]*len(word1))
     if num == len(word1):
         res = res + 50
     return res

dc98cbf94669abf752dd71c629b2977ab692aec4
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-25 13:00:31 +0530

    " -> 4 of 4 passed. pylint: 8/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -6,7 +6,6 @@ import re
 def cleanup(word):
     '''cleaning up'''
     words = []
-    list1 = []
     regex = re.compile('[^a-z],[^A-Z],[^0-9]')
     for w in word:
         n = w.strip('"').strip('.').strip(';').strip(',').strip('"')
@@ -19,17 +18,17 @@ def tokenize(string):
     word = cleanup(string)
     for i in word:
     	if i in word:
-    		dictionary[i] = word.count(i)
+            dictionary[i] = word.count(i)
     return dictionary
-            
+
 def main():
     ''' num: given input'''
     num = int(input())
     list1 = []
     for i in range(num):
-    	list1 = list1 + input().split()
+        i = i + 1
+        list1 = list1 + input().split()
     print(tokenize(list1))
-    
 
 if __name__ == '__main__':
     main()

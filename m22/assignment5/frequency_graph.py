b526fde282801c83cf5ce559815b361bf0184548
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-08-25 12:34:34 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

---
+++ m22/assignment5/frequency_graph.py
@@ -0,0 +1,18 @@
+'''
+Write a function to print a dictionary with the keys in sorted order along with the
+frequency of each word. Display the frequency values using “#” as a text based graph
+'''
+
+def frequency_graph(dictionary):
+    '''keys contains sorted keys.'''
+    keys = sorted(dictionary.keys())
+    for key in keys:
+        print(key, "-", dictionary[key]*'#')
+
+def main():
+	''' dictionary contain dictionary values.'''
+    dictionary = eval(input())
+    frequency_graph(dictionary)
+
+if __name__ == '__main__':
+    main()

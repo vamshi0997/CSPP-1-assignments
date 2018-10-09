cd64a6a641055d343cf13f21f5fd68611247d09e
vamshi0997 <vamshikrishna0997@msitprogram.net>
2018-10-09 17:49:22 +0530

    " -> 3 of 3 passed. pylint: 8/10 "

--- m16/CodeCampDocumentDistance/document_distance.py
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -12,21 +12,17 @@ def similarity(dict1, dict2):
     text2 = cleanup(dict2)
 
     stopwords = load_stopwords("stopwords.txt")
-    
     text1 = remove_stopwords(text1, stopwords)
     text2 = remove_stopwords(text2, stopwords)
-    d = dict()
-    d = create_dictionary(text1, text2, d)
-    
-    return compute(d)
+    diction = dict()
+    diction = create_dictionary(text1, text2, diction)
+    return compute(diction)
 
 def remove_stopwords(word, stopwords):
     words1 = [i for i in word if i not in stopwords and len(i) > 0]
     return words1
 
-    
 def cleanup(doc):
-    
     word = doc.lower()
     word = word.split(" ")
     words1 = []
@@ -38,25 +34,25 @@ def cleanup(doc):
             words1.append((regex.sub("", w)))
     return words1
 
-def create_dictionary(word, word1, d):
+def create_dictionary(word, word1, diction):
     for i in word:
         list1 = []
         list1.append(word.count(i))
         if i not in word1:
             list1.append(0)
-            d[i] = list1
+            diction[i] = list1
         else:
             list1.append(word1.count(i))
-            d[i] = list1
+            diction[i] = list1
     for i in word1:
-        if i not in d:
-            d[i] = [0, word1.count(i)]
-    return d
+        if i not in diction:
+            diction[i] = [0, word1.count(i)]
+    return diction
 
-def compute(d):
-    numerator = sum([i[0]*i[1] for i in d.values()])
-    denominator1 = math.sqrt(sum([i[0]**2 for i in d.values()]))
-    denominator2 = math.sqrt(sum([i[1]**2 for i in d.values()]))
+def compute(diction):
+    numerator = sum([i[0]*i[1] for i in diction.values()])
+    denominator1 = math.sqrt(sum([i[0]**2 for i in diction.values()]))
+    denominator2 = math.sqrt(sum([i[1]**2 for i in diction.values()]))
     return(numerator/(denominator1*denominator2))
 
 def load_stopwords(filename):
